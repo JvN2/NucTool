4d038db7c1734c459fbb59865f404214269d2536
John van Noort <noort@physics.leidenuniv.nl>
2025-10-17 18:15:19 +0200

    start with chromatin fiber class

---
+++ tests/test_chromatin_fiber.py
@@ -0,0 +1,23 @@
+import numpy as np
+from chromatin_fiber import ChromatinFiber
+
+
+def test_occupancy_basic():
+    seq = "A" * 1000
+    cf = ChromatinFiber(seq)
+    cf.place_dyads([100, 300, 700])
+    occ = cf.occupancy(footprint=146)
+    assert occ.shape[0] == len(seq)
+    # check that dyad centers are within occupied windows
+    for d in cf.dyads:
+        assert occ[d] == 1.0
+
+
+def test_sample_configuration():
+    seq = "A" * 500
+    cf = ChromatinFiber(seq)
+    cf.place_dyads([50, 150, 250, 350])
+    res = cf.sample_configuration(footprint=146, rng=np.random.default_rng(42))
+    assert isinstance(res.dyads, np.ndarray)
+    assert isinstance(res.occupancy, np.ndarray)
+    assert res.occupancy.shape[0] == len(seq)
